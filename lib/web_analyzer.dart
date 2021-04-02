4e4bb5216de9f44a73150161f6c75d51704b2f56
nmthanh <madara134@gmail.com>
2021-04-02 16:08:37 +0700

    Cập nhật

--- lib/web_analyzer.dart
+++ lib/web_analyzer.dart
@@ -182,7 +182,7 @@ class WebAnalyzer {
     final ioClient = HttpClient()..badCertificateCallback = _certificateCheck;
     final client = IOClient(ioClient);
     final request = Request('GET', uri)
-      ..followRedirects = false
+      ..followRedirects = true
       ..headers["User-Agent"] = useDesktopAgent
           ? "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
           : "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
